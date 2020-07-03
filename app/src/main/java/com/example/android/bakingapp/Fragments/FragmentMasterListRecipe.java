b9a98e30ff81b7312638a5c78852ced8d40a4fb8
Sabina Leitoiu <sabinaleitoiu91@gmail.com>
2020-07-03 22:30:14 +0100

    Update FragmentMasterListRecipe.java

--- app/src/main/java/com/example/android/bakingapp/Fragments/FragmentMasterListRecipe.java
+++ app/src/main/java/com/example/android/bakingapp/Fragments/FragmentMasterListRecipe.java
@@ -40,7 +40,8 @@ public class FragmentMasterListRecipe extends Fragment
     private static final String TWO_PANE_KEY = "twoPaneMode";
     private static final String POSITION_KEY = "position";
 
-    // --Commented out by Inspection (11/14/2018 8:00 PM):private final String LOG_TAG = FragmentMasterListRecipe.class.getSimpleName();
+    // --Commented out by Inspection (11/14/2018 8:00 PM):private final String 
+    //                  LOG_TAG = FragmentMasterListRecipe.class.getSimpleName();
     public static final String URL_RECIPE_REQUEST = "https://d17h27t6h515a5.cloudfront" + "" +
             ".net/topher/2017/May/59121517_baking/baking.json";
     boolean twoPane;
